eff1c1936b7f15fa950bc4d02f12373f7ceffda5
Rachel <rnovick13@gmail.com>
2018-09-30 17:37:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,13 @@ def valid_move?(board, index)
   if !index.between?(0,8)
     return nil
     elsif position_taken?
+  end
+end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
