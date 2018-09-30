84f88330177d48adae7cbb00133cbc084c66a542
Rachel <rnovick13@gmail.com>
2018-09-30 17:39:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-input_to_index = user_input.
+def input_to_index(user_input)
+  user_input.to_i
+end
 
 def valid_move?(board, index)
   if !index.between?(0,8)
