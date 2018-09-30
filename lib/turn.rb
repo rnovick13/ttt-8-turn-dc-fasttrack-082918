ccd1345938c46001d95cae7186c862d6d15c2215
Rachel <rnovick13@gmail.com>
2018-09-30 18:12:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,8 +37,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   valid_move?(board, index)
-  while valid_move?(board, index) == false
-  turn(board)
+  
 end
 end
 
